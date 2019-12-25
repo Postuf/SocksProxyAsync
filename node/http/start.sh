d36cbafa039a9d443403c7f33900c82cd3cd7b6a
Stanislav V <vorobiev.s@postuf.com>
2019-12-25 11:53:10 +0300

    fix .travis.yml

--- node/http/start.sh
+++ node/http/start.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-../node_modules/http-server/bin/http-server --p 8080
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$DIR/../node_modules/http-server/bin/http-server --p 8080

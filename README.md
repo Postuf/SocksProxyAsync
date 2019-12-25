84e80f1535bc84e72cffa53995d81c2b812dda4b
Stanislav V <vorobiev.s@postuf.com>
2019-12-25 11:00:46 +0300

    add exception test; update readme

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Asynchronous SOCKS5 client library
   * ext-sockets
 
 ## Testing
-Tests require working proxy to be up and running, use `node/proxy.js` for this.
+Tests require working proxy and http server to be up and running, use `node/proxy.js` to start proxy, `node/http/start.sh` (`./start.sh` within its subdir) to start http server.
+By default, http server runs on port 8080, proxy occupies port 1080, tests use these ports.
--- tests/Integration/SocketAsyncTest.php
+++ tests/Integration/SocketAsyncTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Unit;
+namespace Integration;
 
 use PHPUnit\Framework\TestCase;
 use SocksProxyAsync\Proxy;
@@ -16,33 +16,47 @@ class SocketAsyncTest extends TestCase
 
     /** @var SocketAsync */
     private $socket;
+    /** @var Proxy */
+    private $proxy;
 
     protected function setUp(): void
     {
-        $proxy = new Proxy(static::PROXY);
-        $this->socket = new SocketAsync($proxy, self::HOST, self::PORT);
+        $this->proxy = new Proxy(static::PROXY);
+        $this->socket = new SocketAsync($this->proxy, self::HOST, self::PORT);
         parent::setUp();
     }
 
     /** @test
      * @throws SocksException
      */
-    public function test_socket() {
+    public function test_socket_works() {
         $this->assertEquals(self::HOST, $this->socket->getHost());
 
         while(!$this->socket->ready()) {
             $this->socket->poll();
         }
 
+        // http req body
         $br = "\r\n";
         $data = "GET /test{$br}Host: 127.0.0.1:8080{$br}Accept: identity{$br}{$br}";
+
         $writtenBytes = $this->socket->write($data);
         $this->assertEquals($writtenBytes, strlen($data));
         usleep(2000000);
         $response = $this->socket->read(4096);
         $lines = explode("\n", trim($response));
         $lastLine = $lines[count($lines) - 1];
+        /** @see node/http/test */
         $this->assertEquals('test', $lastLine);
         $this->socket->stop();
+    }
+
+    public function test_throw_on_incorrect_port() {
+        // here we assume port 9999 is not occupied
+        $socket = new SocketAsync($this->proxy, self::HOST, 9999);
+        $this->expectException('SocksProxyAsync\SocksException');
+        while(!$this->socket->ready()) {
+            $socket->poll();
+        }
     }
 }

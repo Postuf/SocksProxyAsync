7ec8f414faf68946c3f75c9ea022303b3f07fcaa
Stanislav V <vorobiev.s@postuf.com>
2020-01-10 17:34:36 +0300

    Add dns lookup test

---
+++ tests/Integration/DnsTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Integration;
+
+use PHPUnit\Framework\TestCase;
+use SocksProxyAsync\DNS\dnsAresult;
+use SocksProxyAsync\DNS\dnsMXresult;
+use SocksProxyAsync\DNS\dnsProtocol;
+use SocksProxyAsync\DNS\dnsResponse;
+
+class DnsTest extends TestCase
+{
+    public function test_resolve(): void
+    {
+        $dns = new dnsProtocol();
+        $dns->setServer('8.8.8.8');
+        $question = 'google.com';
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $result = $dns->Query($question,'A');
+        /* @var $result dnsResponse */
+        $found = false;
+        foreach ($result->getResourceResults() as $resource) {
+            if ($resource instanceof dnsAresult) {
+                $found = true;
+                $this->assertEquals($question, $resource->getDomain());
+            }
+        }
+        $this->assertEquals(true, $found);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $result = $dns->Query($question,'MX');
+        /* @var $result dnsResponse */
+        $found = false;
+        foreach ($result->getResourceResults() as $resource) {
+            if ($resource instanceof dnsMXresult) {
+                $found = true;
+                $this->assertEquals($question, $resource->getDomain());
+            }
+        }
+
+        $this->assertEquals(true, $found);
+    }
+}

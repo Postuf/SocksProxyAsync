f2dddcd5bfad00f6d4197cd4cdea4b7f8547f92c
wkejqb <58689118+wkejqb@users.noreply.github.com>
2020-02-18 10:46:44 +0300

    Update Proxy.php

--- src/Proxy.php
+++ src/Proxy.php
@@ -25,7 +25,7 @@ class Proxy
      * Proxy constructor.
      * Proxy formats:
      *      1) host:port
-     *      2) host:port|login:password.
+     *      2) host:port|login:password
      *
      * @param string $serverAndPort
      * @param int    $type

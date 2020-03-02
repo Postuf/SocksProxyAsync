87ffe659de2753ac00435d1882964eec3026dfbc
Stanislav V <vorobiev.s@postuf.com>
2020-03-02 10:56:06 +0300

    Test dns cache

--- tests/Integration/SocketAsyncTest.php
+++ tests/Integration/SocketAsyncTest.php
@@ -112,32 +112,35 @@ class SocketAsyncTest extends TestCase
     public function test_socket_works_with_name(): void
     {
         $this->proxy = new Proxy(static::PROXY_HOSTNAME);
-        $this->socket = new SocketAsync($this->proxy, self::HOSTNAME, self::PORT);
-        $this->assertEquals(self::HOSTNAME, $this->socket->getHost());
+        for ($i = 0; $i < 2; $i++) {
+            $this->socket = new SocketAsync($this->proxy, self::HOSTNAME, self::PORT);
+            $this->assertEquals(self::HOSTNAME, $this->socket->getHost());
 
-        while (!$this->socket->ready()) {
-            $this->socket->poll();
-        }
-
-        // http req body
-        $br = "\r\n";
-        $data = "GET /test{$br}Host: 127.0.0.1:8080{$br}Accept: identity{$br}{$br}";
-
-        $writtenBytes = $this->socket->write($data);
-        $this->assertEquals($writtenBytes, strlen($data));
-        usleep(2000000);
-        $response = $this->socket->read(4096);
-        $lines = explode("\n", trim($response));
-        foreach ($lines as $k => $line) {
-            if ($line && trim($line) === 'Connection: close') {
-                unset($lines[$k]);
+            while (!$this->socket->ready()) {
+                $this->socket->poll();
             }
+
+            // http req body
+            $br = "\r\n";
+            $data = "GET /test{$br}Host: 127.0.0.1:8080{$br}Accept: identity{$br}{$br}";
+
+            $writtenBytes = $this->socket->write($data);
+            $this->assertEquals($writtenBytes, strlen($data));
+            usleep(2000000);
+            $response = $this->socket->read(4096);
+            $lines = explode("\n", trim($response));
+            foreach ($lines as $k => $line) {
+                if ($line && trim($line) === 'Connection: close') {
+                    unset($lines[$k]);
+                }
+            }
+            $lines = array_values($lines);
+            $lastLine = $lines[count($lines) - 1];
+            /* @see node/http/test */
+            $this->assertEquals('test', $lastLine);
+            $this->socket->stop();
+            unset($this->socket);
         }
-        $lines = array_values($lines);
-        $lastLine = $lines[count($lines) - 1];
-        /* @see node/http/test */
-        $this->assertEquals('test', $lastLine);
-        $this->socket->stop();
     }
 
     /**
